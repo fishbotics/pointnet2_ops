12766064ce57af2853f8d13cabe8cf06d869fea3
Adam Fishman <afishman@cs.washington.edu>
2021-04-07 16:54:09 -0700

    Updated again to use TORCH_CHECK instead of internal assert

--- pointnet2_ops/_ext-src/include/utils.h
+++ pointnet2_ops/_ext-src/include/utils.h
@@ -2,25 +2,7 @@
 #include <ATen/cuda/CUDAContext.h>
 #include <torch/extension.h>
 
-#define CHECK_CUDA(x)                                    \
-  do {                                                   \
-    AT_ASSERT(x.is_cuda(), #x " must be a CUDA tensor"); \
-  } while (0)
-
-#define CHECK_CONTIGUOUS(x)                                          \
-  do {                                                               \
-    AT_ASSERT(x.is_contiguous(), #x " must be a contiguous tensor"); \
-  } while (0)
-
-#define CHECK_IS_INT(x)                               \
-  do {                                                \
-    AT_ASSERT(x.scalar_type() == at::ScalarType::Int, \
-              #x " must be an int tensor");           \
-  } while (0)
-
-#define CHECK_IS_FLOAT_OR_HALF(x)                       \
-  do {                                                  \
-    AT_ASSERT(x.scalar_type() == at::ScalarType::Float  \
-              || x.scalar_type == at::ScalarType::Half, \
-              #x " must be a float tensor");            \
-  } while (0)
+#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_IS_INT(x) TORCH_CHECK(x.scalar_type() == at::ScalarType::Int, #x " must be an int tensor")
+#define CHECK_IS_FLOAT_OR_HALF(x) TORCH_CHECK(x.scalar_type() == at::ScalarType::Float || x.scalar_type == at::ScalarType::Half, #x " must be a float tensor")
