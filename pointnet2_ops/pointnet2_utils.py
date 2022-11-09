eb5d72674bdb94470e730f47716b1288c754d0cf
Adam Fishman <afishman@cs.washington.edu>
2022-11-09 11:55:19 -0800

    Removed JIT compiling to help debug compilation issues

--- pointnet2_ops/pointnet2_utils.py
+++ pointnet2_ops/pointnet2_utils.py
@@ -4,31 +4,7 @@ import warnings
 from torch.autograd import Function
 from typing import *
 
-try:
-    import pointnet2_ops._ext as _ext
-except ImportError:
-    from torch.utils.cpp_extension import load
-    import glob
-    import os.path as osp
-    import os
-
-    warnings.warn("Unable to load pointnet2_ops cpp extension. JIT Compiling.")
-
-    _ext_src_root = osp.join(osp.dirname(__file__), "_ext-src")
-    _ext_sources = glob.glob(osp.join(_ext_src_root, "src", "*.cpp")) + glob.glob(
-        osp.join(_ext_src_root, "src", "*.cu")
-    )
-    _ext_headers = glob.glob(osp.join(_ext_src_root, "include", "*"))
-
-    os.environ["TORCH_CUDA_ARCH_LIST"] = "5.2+PTX;7.5;8.6"
-    _ext = load(
-        "_ext",
-        sources=_ext_sources,
-        extra_include_paths=[osp.join(_ext_src_root, "include")],
-        extra_cflags=["-O3"],
-        extra_cuda_cflags=["-O3", "-Xfatbin", "-compress-all"],
-        with_cuda=True,
-    )
+import pointnet2_ops._ext as _ext
 
 
 class FurthestPointSampling(Function):
