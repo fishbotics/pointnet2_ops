8afb2b39d4cbbd9d4414edc04ed2529e211f799e
Adam Fishman <45679966+fishbotics@users.noreply.github.com>
2024-01-25 11:58:49 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ _ext_headers = glob.glob(osp.join(_ext_src_root, "include", "*"))
 
 exec(open(osp.join("pointnet2_ops", "_version.py")).read())
 
-os.environ["TORCH_CUDA_ARCH_LIST"] = "7.0 7.5 8.0 8.6"
+os.environ["TORCH_CUDA_ARCH_LIST"] = "7.0 7.5 8.0 8.6 8.9"
 setup(
     name="pointnet2_ops",
     version=__version__,
